bac8b244947564fa791535c64862a5cc5f6dbc59
kulix <kulixon261@gmail.com>
2024-12-02 23:42:47 +0100

    Add Point class to task 6

--- Zadanie6/main.py
+++ Zadanie6/main.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 class Poly:
     def __init__(self, c=0, n=0):
@@ -89,6 +90,73 @@ class TestPoly(unittest.TestCase):
         p3 = Poly.from_coeffs([1, 2, 4])
         self.assertTrue(p1 == p2)
         self.assertFalse(p1 == p3)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+    
+    def cross(self, other):
+        return self.x * other.y - self.y * other.x
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class TestPoint(unittest.TestCase):
+    def test_str(self):
+        self.assertEqual(str(Point(1, 2)), "(1, 2)")
+
+    def test_repr(self):
+        self.assertEqual(repr(Point(1, 2)), "Point(1, 2)")
+
+    def test_eq(self):
+        self.assertTrue(Point(1, 2) == Point(1, 2))
+        self.assertFalse(Point(1, 2) == Point(2, 3))
+
+    def test_ne(self):
+        self.assertTrue(Point(1, 2) != Point(2, 3))
+        self.assertFalse(Point(1, 2) != Point(1, 2))
+
+    def test_add(self):
+        self.assertEqual(Point(1, 2) + Point(3, 4), Point(4, 6))
+
+    def test_sub(self):
+        self.assertEqual(Point(3, 4) - Point(1, 2), Point(2, 2))
+
+    def test_mul(self):
+        self.assertEqual(Point(1, 2) * Point(3, 4), 11)
+
+    def test_cross(self):
+        self.assertEqual(Point(1, 2).cross(Point(3, 4)), -2)
+
+    def test_length(self):
+        self.assertEqual(Point(3, 4).length(), 5)
+
+    def test_hash(self):
+        self.assertEqual(hash(Point(1, 2)), hash((1, 2)))
+
 
 if __name__ == '__main__':
     unittest.main()
